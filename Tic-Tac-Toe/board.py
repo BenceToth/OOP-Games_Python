5a297eed1ece2f29855254f800bbb0b11b57710e
Bence L Toth <bence.l.toth@gmail.com>
2024-08-08 12:41:57 +0200

    add function to check if a player won

--- Tic-Tac-Toe/board.py
+++ Tic-Tac-Toe/board.py
@@ -38,3 +38,50 @@ class Board:
             self.game_board[row][column] = player.marker
         else:
             print("This position is already taken. Please enter another one")
+            
+    def check_is_game_over(self, player, last_move):
+        return ((self.check_row(player, last_move)
+                 or (self.check_column(player, last_move))
+                 or (self.check_diagonal(player))
+                 or (self.check_antidiagonal(player))))
+    
+    def check_row(self, player, last_move):
+        """
+        Checks if the player's marker appears 3 times in the row
+        of the last move
+        """
+        row_index = last_move.get_row()
+        board_row = self.game_board[row_index]  # e.g.: ["O", 0, "X"]
+        
+        return board_row.count(player.marker) == 3
+    
+    def check_column(self, player, last_move):
+        """
+        Checks if the player's marker appears 3 times in the column
+        of the last move
+        """
+        markers_count = 0
+        column_index = last_move.get_column()
+        
+        for i in range(3):
+            if self.game_board[i][column_index] == player.marker:
+                markers_count += 1
+                
+        return markers_count == 3
+    
+    def check_diagonal(self, player):
+        markers_count = 0
+        for i in range(3):
+            if self.game_board[i][i] == player.marker:
+                markers_count +=1
+                
+        return markers_count == 3
+    
+    def check_antidiagonal(self, player):
+        markers_count = 0
+        
+        for i in range(3):
+            if self.game_board[i][2-i] == player.marker:
+                markers_count += 1
+                
+        markers_count == 3
