1e8c3445c66977139a482eb1d12fdb7c4da9bf50
Bence L Toth <bence.l.toth@gmail.com>
2024-08-22 14:19:53 +0200

    write insert_node() method - beginning of chain

--- Linked-List/linked_list.py
+++ Linked-List/linked_list.py
@@ -5,3 +5,15 @@ class LinkedList:
 
     def __init__(self):
         self.head = None
+        
+    def insert_node(self, value):
+        new_node = Node(value)
+        
+        # if the LinkedList is empty
+        if self.head is None:
+            self.head = new_node
+        # keeping ascending order
+        elif value <= self.head.value:
+            # change pointer
+            new_node.next = self.head
+            self.head = new_node
