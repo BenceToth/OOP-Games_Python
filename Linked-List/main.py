7da15b98445b722eaa49c6e019c5946480b4dd71
Bence L Toth <bence.l.toth@gmail.com>
2024-08-22 14:42:46 +0200

    test with strings

--- Linked-List/main.py
+++ Linked-List/main.py
@@ -18,3 +18,16 @@ print(((my_linked_list.head).next).value)  # value after head: 6
 # 4th: 3 -> 6 -> 9 -> 15
 my_linked_list.insert_node(15)
 print(my_linked_list.head.next.next.next.value)  # 4th value: 15
+
+# Testing with strings
+string_linked_list = LinkedList()
+string_linked_list.insert_node("Python")
+string_linked_list.insert_node("World")
+string_linked_list.insert_node("Hello")
+string_linked_list.insert_node("Code")
+
+# Code -> Hello -> Python -> World
+print(string_linked_list.head.value, end=" ")
+print(string_linked_list.head.next.value, end=" ")
+print(string_linked_list.head.next.next.value, end=" ")
+print(string_linked_list.head.next.next.next.value, end=" ")
